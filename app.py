7ddbf45e944e67642bbbc51bc432de55dc5457b9
Jessica Woolard <jessicawoolard@Jessicas-MacBook-Pro.local>
2019-01-11 15:30:55 -0500

    got toggle working

--- app.py
+++ app.py
@@ -1,9 +1,32 @@
 from flask import Flask
-from flas import request
+from flask import request
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
 
-    request.args.get()
+   position = request.args.get("position")
+   print(position)
+
+   left_class = "timeline-item_left"
+   right_class = "timeline-item_right"
+
+   if position == 'right':
+       left_class =  "timeline-item_right"
+       right_class = "timeline-item_left"
+
+    # open file for reading, 'r'
+   # file is saved to variable
+   index_file = open('index.html', 'r')
+   # read contents of the file
+   my_html = index_file.read()
+
+   #     # add use input back into the html
+   my_html = my_html.replace("{{timeline-item_left}}", left_class)
+   my_html = my_html.replace("{{timeline-item_right}}", right_class)
+
+   # close the file out when you're done
+   index_file.close()
+
+   return my_html
